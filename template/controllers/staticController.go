083778e6684802d4377fa942ef0391d93315bc4a
huzhouyu <a1085052074@qq.com>
2019-12-26 21:29:23 +0800

    优化静态资源处理

--- template/controllers/staticController.go
+++ template/controllers/staticController.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"github.com/freefishgo/freeFishGo/middlewares/mvc"
+	"io"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"{{[.ProjectName]}}/fishgo"
 )
@@ -25,8 +27,8 @@ type data struct {
 //
 // 由于重写了该动作的路由为：static/{path:allString}  所以d.Path 即为请求url中出 static/ 后面的部分
 func (static *staticController) StaticFile(d *data) {
-	if b, err := ioutil.ReadFile(filepath.Join("static", d.Path)); err == nil {
-		static.Response.Write(b)
+	if f, err := os.Open(filepath.Join("static", d.Path)); err == nil {
+		io.Copy(static.Response,f)
 	} else {
 		static.Response.WriteHeader(404)
 		static.Response.Write([]byte(err.Error()))
