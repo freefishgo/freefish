7806b68b06042a2cb31b5dfed8541381bcdc0bfd
huzhouyu <a1085052074@qq.com>
2019-12-28 14:23:50 +0800

    修正备注说明

--- template/controllers/homeController.go
+++ template/controllers/homeController.go
@@ -14,10 +14,10 @@ func init() {
 	mvc.AddHandlers(&HomeController{})
 }
 
-// MyControllerActionStrut为{Action}的值 该方法的默认路由为/Home/MyControllerActionStrut 最后的单词为请求方式  该例子为Post请求
+// Index为{Action}的值 该方法的默认路由为/Home/Index 最后的单词为请求方式  该例子为Post请求
 func (c *HomeController) Index() {
 	c.Data["Website"] = "freefishgo.com"
 	c.Data["Email"] = "a1085052074@qq.com"
-	// 调用模板引擎   默认模板地址为{ Controller}/{Action}.fish    即为Main/MyControllerActionStrut， c.UseTplPath()等效于c.UseTplPath("Home/MyControllerActionStrut")
+	// 调用模板引擎   默认模板地址为{ Controller}/{Action}.fish    即为Home/Index， c.UseTplPath()等效于c.UseTplPath("Home/Index")
 	c.UseTplPath()
 }
