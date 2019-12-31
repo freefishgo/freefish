3fcd1ebcf2d5d6b3f771940c708bf985489e191c
huzhouyu <huzhouyu@eatown.com.cn>
2019-12-31 12:29:37 +0800

    优化生成控制器

--- main.go
+++ main.go
@@ -315,12 +315,12 @@ func ({{[Name]}} *{{[Controller]}}) OverwriteRouter() []*mvc.ControllerActionRou
 }
 
 // 控制器执行前调用
-func (c *Controller) Prepare() {
+func ({{[Name]}} *{{[Controller]}}) Prepare() {
 	//log.Println("子类的Prepare")
 }
 
 // 控制器结束时调用
-func (c *Controller) Finish() {
+func ({{[Name]}} *{{[Controller]}}) Finish() {
 	//log.Println("子类的Finish")
 }`
 )
