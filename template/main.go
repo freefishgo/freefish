18e1d3938cf2fef050328d795970cb09057f6657
huzhouyu <a1085052074@qq.com>
2020-01-03 23:10:11 +0800

    美化一下代码

--- template/main.go
+++ template/main.go
@@ -16,12 +16,5 @@ func main() {
 	// 把mvc实例注册到管道中
 	freefishgo.UseMiddleware(mvc.DefaultMvcApp)
 	freefishgo.DefaultConfig.Listen.HTTPPort = 8080
-	err := freefishgo.Run()
-	for {
-		select {
-		case e := <-err:
-			panic(e)
-
-		}
-	}
+	freefishgo.Run()
 }
