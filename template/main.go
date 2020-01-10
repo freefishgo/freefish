f32fb1974de4ac78b67bb340d4c8a534ecb0d70b
huzhouyu <a1085052074@qq.com>
2020-01-10 22:03:01 +0800

    美化一下代码

--- template/main.go
+++ template/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/freefishgo/freefishgo"
 	"github.com/freefishgo/freefishgo/middlewares/mvc"
 	"github.com/freefishgo/freefishgo/middlewares/printTimeMiddleware"
-	_ "{{[.ProjectName]}}/conf"
+	"os"
 	_ "{{[.ProjectName]}}/routers"
 )
+
+type config struct {
+	*freefishgo.Config
+	WebConfig *mvc.MvcWebConfig
+}
+
+func init() {
+	conf := new(config)
+	f, err := os.Open("conf/app.conf")
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	json.NewDecoder(f).Decode(conf)
+	freefishgo.SetDefaultApplicationBuilderConfig(conf.Config)
+	mvc.SetDefaultMvcWebConfig(conf.WebConfig)
+
+}
 func main() {
 	// 通过注册中间件来打印任务处理时间服务
 	freefishgo.UseMiddleware(&printTimeMiddleware.PrintTimeMiddleware{})
