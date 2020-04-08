8c4c94005f7b5b2596e2e6983c8e555cc9950be8
freefishgo <a1085052074@qq.com>
2020-04-08 14:22:04 +0800

    美化代码

--- template/controllers/homeController.go
+++ template/controllers/homeController.go
@@ -5,19 +5,19 @@ import (
 )
 
 // 实现mvc控制器的处理Main为控制器 {Controller}的值
-type HomeController struct {
+type homeController struct {
 	mvc.Controller
 }
 
 // 注册控制器
 func init() {
-	mvc.AddHandlers(&HomeController{})
+	mvc.AddHandlers(&homeController{})
 }
 
-// Index为{Action}的值 该方法的默认路由为/Home/Index 最后的单词为请求方式  默认为Get  如需设置为post 例：IndexPost
-func (home *HomeController) Index() {
+// Index为{Action}的值 该方法的默认路由为/home/Index 最后的单词为请求方式  默认为Get  如需设置为post 例：IndexPost
+func (home *homeController) Index() {
 	home.Data["Website"] = "freefishgo.com"
 	home.Data["Email"] = "a1085052074@qq.com"
-	// 调用模板引擎   默认模板地址为{ Controller}/{Action}.fish    即为Home/Index， home.UseTplPath()等效于c.UseTplPath("Home/Index")
+	// 调用模板引擎   默认模板地址为{ Controller}/{Action}.fish    即为Home/Index， home.UseTplPath()等效于c.UseTplPath("home/Index")
 	home.UseTplPath()
 }
--- template/routers/router.go
+++ template/routers/router.go
@@ -9,6 +9,6 @@ func init() {
 	// 注册主路由  可多主路由格式      但 主页面 设置只有第一个有效
 	mvc.AddMainRouter(&mvc.MainRouter{
 		RouterPattern:  "/{ Controller}/{Action}",
-		HomeController: "Home",
+		HomeController: "home",
 		IndexAction:    "Index"})
 }
